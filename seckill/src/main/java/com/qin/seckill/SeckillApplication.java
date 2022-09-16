d6487cd07498f70046c0bd40cc47e9b9b98a0ecb
qinchunabng <373413704@qq.com>
2022-09-16 22:06:06 +0800

    first commit

---
+++ seckill/src/main/java/com/qin/seckill/SeckillApplication.java
@@ -0,0 +1,18 @@
+package com.qin.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description
+ *
+ * @author qcb
+ * @date 2022/09/16 22:02.
+ */
+@SpringBootApplication
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+}
